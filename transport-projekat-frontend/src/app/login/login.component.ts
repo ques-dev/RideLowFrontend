1ae254670899123ca9e318db4797d65e05aa5cf6
Maja <maya.grudge.12@gmail.com>
2022-12-14 19:02:31 +0100

    Added design for login component. Integrated it to the joined registration/login component

--- transport-projekat-frontend/src/app/login/login.component.ts
+++ transport-projekat-frontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css','../app.component.css']
 })
 export class LoginComponent {
-
+  loginForm = new FormGroup({
+    email: new FormControl( '',[Validators.required]),
+    password: new FormControl('',[Validators.required]),
+  });
 }
