65b2f580b09da2c5388fa94509089f1287aee9b3
Vuk Radmilović <vuk.radmilovic01@gmail.com>
2022-12-17 19:48:09 +0100

    Add taken and free car symbols and a legend to bottom right of the map
    Refactor map.component.ts to be more clean.

--- transport-projekat-frontend/src/app/map/map.component.ts
+++ transport-projekat-frontend/src/app/map/map.component.ts
@@ -1,6 +1,12 @@
 import {AfterViewInit, Component} from '@angular/core';
 import * as L from 'leaflet';
 import 'leaflet-routing-machine';
+import {LatLngTuple} from "leaflet";
+
+const CIRCLE_FILL_OPACITY = 0.9;
+const CIRCLE_RADIUS = 20;
+const FREE_CIRCLE_COLOR = '#50B214';
+const TAKEN_CIRCLE_COLOR = '#FD1C1C';
 
 @Component({
   selector: 'app-map',
@@ -8,7 +14,9 @@ import 'leaflet-routing-machine';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements AfterViewInit {
-  private map: L.Map | undefined;
+  private map!: L.Map;
+  private takenCars: LatLngTuple[] = [[45.240174, 19.837885], [45.236360, 19.836721], [45.237863, 19.829511], [45.243302, 19.825220]];
+  private freeCars: LatLngTuple[] = [[45.237002, 19.829361], [45.240477, 19.847849], [45.244125, 19.842828], [45.246484, 19.840132]];
 
   private startMarker: L.Marker = L.marker([45.245932, 19.850961], {
     draggable: true,
@@ -32,7 +40,9 @@ export class MapComponent implements AfterViewInit {
     });
 
     tiles.addTo(this.map);
+  }
 
+  private initRouting(): void {
     L.Routing.control({
       router: L.Routing.osrmv1({
         serviceUrl: `http://router.project-osrm.org/route/v1/`
@@ -58,7 +68,36 @@ export class MapComponent implements AfterViewInit {
     }).addTo(this.map);
   }
 
+  private initTakenCars(): void {
+    for (const car of this.takenCars) {
+      L.circle(car, {
+        color: TAKEN_CIRCLE_COLOR,
+        fillColor: TAKEN_CIRCLE_COLOR,
+        fillOpacity: CIRCLE_FILL_OPACITY,
+        radius: CIRCLE_RADIUS
+      }).addTo(this.map);
+    }
+  }
+
+  private initFreeCars(): void {
+    for (const car of this.freeCars) {
+      L.circle(car, {
+        color: FREE_CIRCLE_COLOR,
+        fillColor: FREE_CIRCLE_COLOR,
+        fillOpacity: CIRCLE_FILL_OPACITY,
+        radius: CIRCLE_RADIUS
+      }).addTo(this.map);
+    }
+  }
+
+  private initCars(): void {
+    this.initTakenCars();
+    this.initFreeCars();
+  }
+
   ngAfterViewInit(): void {
     this.initMap();
+    this.initRouting();
+    this.initCars();
   }
 }
