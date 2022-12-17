7a955c687eeedafb8b82fac1b274dbbc8ba6042d
Vuk Radmilović <vuk.radmilovic01@gmail.com>
2022-12-17 18:34:13 +0100

    Add starting and ending pins as well as routing to the map
    Make map container not use position: absolute

--- transport-projekat-frontend/src/app/map/map.component.ts
+++ transport-projekat-frontend/src/app/map/map.component.ts
@@ -1,5 +1,6 @@
 import {AfterViewInit, Component} from '@angular/core';
 import * as L from 'leaflet';
+import 'leaflet-routing-machine';
 
 @Component({
   selector: 'app-map',
@@ -9,10 +10,19 @@ import * as L from 'leaflet';
 export class MapComponent implements AfterViewInit {
   private map: L.Map | undefined;
 
+  private startMarker: L.Marker = L.marker([45.245932, 19.850961], {
+    draggable: true,
+    title: 'Start'
+  });
+  private endMarker: L.Marker = L.marker([45.238844, 19.838659], {
+    draggable: true,
+    title: 'End'
+  });
+
   private initMap(): void {
     this.map = L.map('map', {
-      center: [ 39.8282, -98.5795 ],
-      zoom: 3
+      center: [ 45.245932, 19.850961 ],
+      zoom: 15
     });
 
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -22,6 +32,30 @@ export class MapComponent implements AfterViewInit {
     });
 
     tiles.addTo(this.map);
+
+    L.Routing.control({
+      router: L.Routing.osrmv1({
+        serviceUrl: `http://router.project-osrm.org/route/v1/`
+      }),
+      showAlternatives: true,
+      lineOptions: {
+        styles: [{color: '#242c81', weight: 7}],
+        extendToWaypoints: true,
+        missingRouteTolerance: 100
+      },
+      fitSelectedRoutes: false,
+      altLineOptions: {
+        styles: [{color: '#ed6852', weight: 7}],
+        extendToWaypoints: true,
+        missingRouteTolerance: 100
+      },
+      show: false,
+      routeWhileDragging: true,
+      waypoints: [
+        this.startMarker.getLatLng(),
+        this.endMarker.getLatLng()
+      ]
+    }).addTo(this.map);
   }
 
   ngAfterViewInit(): void {
