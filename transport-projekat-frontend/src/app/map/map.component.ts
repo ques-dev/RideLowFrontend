7f1332ae0c2c7fe502fcfefbfbd1776a09371124
Maja <maya.grudge.12@gmail.com>
2022-12-18 16:22:47 +0100

    Added custom location and vehicle icons, changed legend, couple design changes.

--- transport-projekat-frontend/src/app/map/map.component.ts
+++ transport-projekat-frontend/src/app/map/map.component.ts
@@ -11,32 +11,69 @@ const TAKEN_CIRCLE_COLOR = '#FD1C1C';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.css','../app.component.css']
 })
 export class MapComponent implements AfterViewInit {
   private map!: L.Map;
   private takenCars: LatLngTuple[] = [[45.240174, 19.837885], [45.236360, 19.836721], [45.237863, 19.829511], [45.243302, 19.825220]];
   private freeCars: LatLngTuple[] = [[45.237002, 19.829361], [45.240477, 19.847849], [45.244125, 19.842828], [45.246484, 19.840132]];
 
+  private locationIcon = L.icon({
+    iconUrl: 'assets/images/place-marker.png',
+    shadowUrl: 'assets/images/place-marker.png',
+    iconRetinaUrl : 'assets/images/place-marker.png',
+
+    iconSize:     [40, 40], // size of the icon
+    shadowSize:   [0,0], // size of the shadow
+    iconAnchor:   [20, 35], // point of the icon which will correspond to marker's location
+    shadowAnchor: [0, 0],  // the same for the shadow
+    popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
+  });
+
+  private vehicleFreeIcon = L.icon({
+    iconUrl: 'assets/images/vehicle-green.png',
+    shadowUrl: 'assets/images/vehicle-green.png',
+    iconRetinaUrl : 'assets/images/vehicle-green.png',
+
+    iconSize:     [25, 25], // size of the icon
+    shadowSize:   [0,0], // size of the shadow
+    iconAnchor:   [15, 20], // point of the icon which will correspond to marker's location
+    shadowAnchor: [0, 0],  // the same for the shadow
+    popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
+  });
+
+  private vehicleReservedIcon = L.icon({
+    iconUrl:'assets/images/vehicle-red.png',
+    shadowUrl: 'assets/images/vehicle-red.png',
+    iconRetinaUrl : 'assets/images/vehicle-red.png',
+
+    iconSize:     [25, 25], // size of the icon
+    shadowSize:   [0,0], // size of the shadow
+    iconAnchor:   [15, 20], // point of the icon which will correspond to marker's location
+    shadowAnchor: [0, 0],  // the same for the shadow
+    popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
+  });
+
   private startMarker: L.Marker = L.marker([45.245932, 19.850961], {
     draggable: true,
-    title: 'Start'
+    title: 'Start',
   });
   private endMarker: L.Marker = L.marker([45.238844, 19.838659], {
     draggable: true,
-    title: 'End'
+    title: 'End',
   });
 
   private initMap(): void {
     this.map = L.map('map', {
       center: [ 45.245932, 19.850961 ],
-      zoom: 15
+      zoom: 15,
+      zoomControl : false,
+      attributionControl : false
     });
 
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 18,
       minZoom: 3,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     });
 
     tiles.addTo(this.map);
@@ -49,7 +86,7 @@ export class MapComponent implements AfterViewInit {
       }),
       showAlternatives: true,
       lineOptions: {
-        styles: [{color: '#242c81', weight: 7}],
+        styles: [{color: '#CF0A0A', weight: 7}],
         extendToWaypoints: true,
         missingRouteTolerance: 100
       },
@@ -70,22 +107,18 @@ export class MapComponent implements AfterViewInit {
 
   private initTakenCars(): void {
     for (const car of this.takenCars) {
-      L.circle(car, {
-        color: TAKEN_CIRCLE_COLOR,
-        fillColor: TAKEN_CIRCLE_COLOR,
-        fillOpacity: CIRCLE_FILL_OPACITY,
-        radius: CIRCLE_RADIUS
+      L.marker(car, {
+        draggable: false,
+        icon: this.vehicleReservedIcon
       }).addTo(this.map);
     }
   }
 
   private initFreeCars(): void {
     for (const car of this.freeCars) {
-      L.circle(car, {
-        color: FREE_CIRCLE_COLOR,
-        fillColor: FREE_CIRCLE_COLOR,
-        fillOpacity: CIRCLE_FILL_OPACITY,
-        radius: CIRCLE_RADIUS
+      L.marker(car, {
+        draggable: false,
+        icon: this.vehicleFreeIcon
       }).addTo(this.map);
     }
   }
@@ -96,6 +129,7 @@ export class MapComponent implements AfterViewInit {
   }
 
   ngAfterViewInit(): void {
+    L.Marker.prototype.options.icon = this.locationIcon;
     this.initMap();
     this.initRouting();
     this.initCars();
--- transport-projekat-frontend/src/app/unregistered-user-main/unregistered-user-main.component.ts
+++ transport-projekat-frontend/src/app/unregistered-user-main/unregistered-user-main.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-unregistered-user-main',
   templateUrl: './unregistered-user-main.component.html',
-  styleUrls: ['./unregistered-user-main.component.css']
+  styleUrls: ['./unregistered-user-main.component.css','../app.component.css']
 })
 export class UnregisteredUserMainComponent {
 
