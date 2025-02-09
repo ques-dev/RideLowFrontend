c1e6d128b1f981d6ac9a478e29cb287db29dc8ec
ques-dev <gqbutt43@gmail.com>
2025-02-09 13:51:00 +0500

    Update environment.ts

--- uber-like-frontend/src/app/environments/environment.ts
+++ uber-like-frontend/src/app/environments/environment.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  apiURL: "http://localhost:8080/api/"
+ // apiURL: "http://localhost:8080/api/"
+ apiURL: "https://tranquil-florentine-d5b212.netlify.app/api/"
+
+    
+  
 }
